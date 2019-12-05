fc12e43a820fc12b7d1872780c9150564b7a93f1
BSFishy <mttprvstanddad@gmail.com>
2019-12-05 21:20:00 +0000

    Suppress warnings

--- runtime/Cpp/runtime/src/atn/ATNState.h
+++ runtime/Cpp/runtime/src/atn/ATNState.h
@@ -1,4 +1,4 @@
-﻿/* Copyright (c) 2012-2017 The ANTLR Project. All rights reserved.
+/* Copyright (c) 2012-2017 The ANTLR Project. All rights reserved.
  * Use of this file is governed by the BSD 3-clause license that
  * can be found in the LICENSE.txt file in the project root.
  */
@@ -70,7 +70,7 @@ namespace atn {
   ///
   /// <embed src="images/OptionalNonGreedy.svg" type="image/svg+xml"/>
   /// </summary>
-  class ANTLR4CPP_PUBLIC ATN;
+  class ATN;
 
   class ANTLR4CPP_PUBLIC ATNState {
   public:
